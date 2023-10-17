6ac359441e1122b2a0684aeb2d821a9d09f04551
Noah <burghardtnoah@gmail.com>
2023-10-17 06:02:30 -0600

    implemented data save/load

--- app/__init__.py
+++ app/__init__.py
@@ -4,31 +4,13 @@ from app import views
 from app import data
 from app.data import Data
 
-def MockData(state: Data):
-    g1 = state.add_group("frequent sites")
-    state.add_mark(g1.id, "google", "https://google.com")
-    state.add_mark(g1.id, "facebook", "https://www.facebook.com")
-    state.add_mark(g1.id, "reddit", "old.reddit.com")
-
-    g2 = state.add_group("home server")
-    state.add_mark(g2.id, "infisical", "infisical.gabbro-ce").icon = "fa-solid fa-infinity"
-    state.add_mark(g2.id, "plex", "plex.tv/web").icon = "fa-solid fa-play"
-    state.add_mark(g2.id, "qbittorrent", "http://deluge:8080").icon = "fa-solid fa-download"
-
-    g3 = state.add_group("videos")
-    state.add_mark(g3.id, "youtube", "https://youtube.com").icon = "fa-brands fa-youtube"
-    state.add_mark(g3.id, "netflix", "netflix.com").icon = "fa-solid fa-n"
-    state.add_mark(g3.id, "twitch", "twitch.tv").icon = "fa-brands fa-twitch"
-
 def Helios(config):
     if 'data_file' not in config:
         config['data_file'] = './data.json'
+    d = Data(config)
+    d.load()
 
     helios = Flask(__name__, template_folder='views/templates', static_folder='views/static')
-
-    d = Data(config)
-    MockData(d)
     views.apply(helios, d)
 
     return helios
-        
--- app/data/__init__.py
+++ app/data/__init__.py
@@ -9,36 +9,118 @@ class Mark:
         self.name  = ""
         self.highlight  = ""
         self.icon = ""
+    def __str__(self):
+        s = ''
+        s += f'id: {self.id}\n'
+        s += f'group_id: {self.group_id}\n'
+        s += f'url: {self.url}\n'
+        s += f'name: {self.name}\n'
+        s += f'highlight: {self.highlight}\n'
+        s += f'icon: {self.icon}'
+        return s
+    def from_json(self, d):
+        self.id = d['id'] if 'id' in d else self.id
+        self.group_id = d['group_id'] if 'group_id' in d else self.group_id
+        self.url = d['url'] if 'url' in d else self.url
+        self.name = d['name'] if 'name' in d else self.name
+        self.highlight = d['highlight'] if 'highlight' in d else self.highlight
+        self.icon = d['icon'] if 'icon' in d else self.icon
+
 class Group:
     def __init__(self):
         self.mark_ids = []
         self.id = ""
         self.name = ""
+    def __str__(self):
+        s = f'id: {self.id}\n'
+        s = f'name: {self.name}\n'
+        s = f'mark_ids:\n'
+        s += '\n'.join([f'  {i}' for i in self.mark_ids])
+        return s
+    def from_json(self, d):
+        self.mark_ids = [str(i) for i in d['mark_ids']] if 'mark_ids' in d else self.mark_ids
+        self.id = d['id'] if 'id' in d else self.id
+        self.name = d['name'] if 'name' in d else self.name
+
 class Colors:
     def __init__(self):
         self.background  = ""
         self.foreground  = ""
+    def __str__(self):
+        s = f'background: {self.background}\n'
+        s += f'foreground: {self.foreground}'
+        return s
+    def from_json(self, d):
+        self.background = d['background'] if 'background' in d else self.background
+        self.foreground = d['foreground'] if 'foreground' in d else self.foreground
+    
 class Settings:
     def __init__(self):
         self.colors = Colors()
         self.search_delay_ms = 100
+    def __str__(self):
+        s = 'colors:\n'
+        s += '\n'.join([f'  {i}' for i in str(self.colors).split('\n')]) + '\n'
+        s += f'search_delay: {self.search_delay_ms}'
+        return s
+    def from_json(self, d):
+        self.colors.from_json(d['colors']) if 'colors' in d else self.colors
+        self.search_delay_ms = int(d['search_delay_ms']) if 'search_delay_ms' in d else self.search_delay_ms
+
 class State:
     def __init__(self):
         self.groups = {}
         self.marks = {}
         self.group_ids = []
         self.settings = Settings()
+    def __str__(self):
+        s = 'settings:\n'
+        s += '\n'.join([f'  {i}' for i in str(self.settings).split('\n')]) + '\n'
+        s += 'group_ids:\n'
+        s += '\n'.join([f'  {i}' for i in self.group_ids]) + '\n'
+        s += 'groups:\n'
+        for gid in self.groups:
+            s += f'{gid}:\n'
+            s += '\n'.join([f'  {i}' for i in str(self.groups[gid]).split('\n')]) + '\n'
+        s += 'marks:\n'
+        for mid in self.marks:
+            s += f'{mid}:\n'
+            s += '\n'.join([f'  {i}' for i in str(self.marks[mid]).split('\n')]) + '\n'
+        return s
+    def from_json(self, d):
+        if 'groups' in d:
+            self.groups = {}
+            for gid in d['groups']:
+                g = Group()
+                g.from_json(d['groups'][gid])
+                self.groups[gid] = g
+        if 'marks' in d:
+            self.marks = {}
+            for mid in d['marks']:
+                m = Mark()
+                m.from_json(d['marks'][mid])
+                self.marks[mid] = m
+        if 'group_ids' in d:
+            self.group_ids = [str(i) for i in d['group_ids']]
+        if 'settings' in d:
+            self.settings = Settings()
+            self.settings.from_json(d['settings'])
+
+
 
 class Data:
     def __init__(self, config):
         self.state = State()
-        pass 
+        self.data_file = config['data_file']
     def get_json(self):
         return json.dumps(self.__dict__, default=lambda o: o.__dict__)
     def save(self):
-        pass
+        with open(self.data_file, 'w') as f:
+            f.write(self.get_json())
     def load(self):
-        pass
+        with open(self.data_file, 'r') as f:
+            self.state = State()
+            self.state.from_json(json.loads(f.read()))
     def add_group(self, name):
         g = Group()
         g.id = str(uuid.uuid4())
